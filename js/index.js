c21eaa2195a25fa37ff32bdbcd9fdcdc9b870b36
Jennifer Xie <jenniferx@berkeley.edu>
2018-08-31 00:16:32 -0700

    Updated images, fixed issues on home page

--- js/index.js
+++ js/index.js
@@ -3,23 +3,33 @@ wrapper = document.getElementsByClassName("wrapper")[0]
 
 wrapper.addEventListener('scroll', function() {
 	var nav = document.getElementById("navbar")
-	if (wrapper.scrollTop <= 20) {
+	if (wrapper.scrollTop <= 40) {
 		nav.className = '';
 	} else {
 		nav.className = 'scroll';
 	}
 });
 
-// Set height of static sections
+// Set height of sections
+
+// Work
 var midSection = document.getElementsByClassName("mid")[0]
 var workTitle = document.getElementById("work-title")
 var smallGrid = document.getElementsByClassName("small-grid")[0]
-
 var midHeight = smallGrid.clientHeight + workTitle.clientHeight
 midSection.style.height = midHeight + "px"
+
+// Contact
+var endSection = document.getElementsByClassName("end")[0]
+var contactTitle = document.getElementById("contact-title")
+var contactGrid = document.getElementsByClassName("contact-grid")[0]
+var endHeight = contactGrid.clientHeight + contactTitle.clientHeight
+endSection.style.height = endHeight + "px"
 
 window.addEventListener('resize', function() {
 	var midHeight = smallGrid.clientHeight + workTitle.clientHeight
 	midSection.style.height = midHeight + "px"
-});
 
+	var endHeight = contactGrid.clientHeight + contactTitle.clientHeight
+	endSection.style.height = endHeight + "px"
+});
